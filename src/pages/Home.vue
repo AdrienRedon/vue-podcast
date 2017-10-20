02013409ea639037128234ffaa4d92f4d632ea89
Adrien REDON <AdrienRedon@users.noreply.github.com>
2017-10-20 09:31:00 +0200

    CLN: trailing spaces

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@
       <li v-for="video in store.videos">
         <a @click="play(video.id)">{{video.title}}</a>
         <a v-if="video.id == store.current && !store.pause" @click="pause()">⏸</a>
-        <a v-else @click="play(video.id)">▶️</a>          
+        <a v-else @click="play(video.id)">▶️</a>
       </li>
     </ul>
   </div>
