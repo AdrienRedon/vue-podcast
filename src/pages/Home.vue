a43268a8342c432ecbe11f9c0634c520a1a4cb12
Adrien REDON <AdrienRedon@users.noreply.github.com>
2017-11-19 16:45:55 +0100

    REF: strict compiraison

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@
     <ul>
       <li v-for="video in store.videos">
         <a @click="play(video.id)">{{video.title}}</a>
-        <a v-if="video.id == store.current && !store.pause" @click="pause()">⏸</a>
+        <a v-if="video.id === store.current && !store.pause" @click="pause()">⏸</a>
         <a v-else @click="play(video.id)">▶️</a>
       </li>
     </ul>
